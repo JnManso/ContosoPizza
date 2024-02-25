377deb1f8fc241aa0a20ab3d0d949469fac909ab
Joao Manso <jnmanso@gmail.com>
2024-02-25 22:59:57 +0000

    Added 2 Examples using Contoso Pizza

---
+++ Example2/Program.cs
@@ -0,0 +1,32 @@
+using ContosoPizza.Data;
+using ContosoPizza.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSqlite<PizzaContext>("Data Source=ContosoPizza.db");
+builder.Services.AddSqlite<PromotionsContext>("Data Source=Promotions/Promotions.db");
+
+builder.Services.AddScoped<PizzaService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.CreateDbIfNotExists();
+
+app.MapGet("/", () => @"Contoso Pizza management API. Navigate to /swagger to open the Swagger test UI.");
+
+app.Run();
---
+++ README.md
@@ -0,0 +1,7 @@
+# Contoso Pizza Examples
+
+- Example 1 - Create a web API with ASP.NET Core controllers
+    https://learn.microsoft.com/en-us/training/modules/build-web-api-aspnet-core/
+
+- Example 2 - Persist and retrieve relational data by using Entity Framework Core
+    https://learn.microsoft.com/en-us/training/modules/persist-data-ef-core/
